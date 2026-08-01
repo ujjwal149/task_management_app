53c7a30983c877350d89e8058e2de20cdf726cfd
Ujjwal Kumar <ujjwal14920@gmail.com>
2026-08-01 15:47:32 +0530

    refactor(client): use relative API URL for production

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "http://localhost:5000/api",
+    "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
